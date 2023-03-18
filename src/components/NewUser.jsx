bb32284939195de8cd44839bc648a73c6335a9f0
Roman-cas88 <orekhovskiy.roman@gmail.com>
2023-03-18 06:52:58 +0100

    add id generator nanoid

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -3,30 +3,59 @@ import { Button, Container } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
 import { addUser } from '../slices/users'
 import { InputForm } from './InputForm'
+import { nanoid } from '@reduxjs/toolkit'
 
 export const NewUser = () => {
     const dispatch = useDispatch()
 
     const addToData = (e) => {
-            e.preventDefault();
-            const formData = new FormData(e.target)
-            let newUser = Object.fromEntries(formData.entries())
-            newUser.id = "002";
-            console.log(newUser);
-            // dispatch(addUser(newUser));
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const newUser = Object.fromEntries(formData.entries())
+        newUser.id = nanoid();
+        dispatch(addUser(newUser));
     }
+
     return (
         <Container>
             <h2> New user: </h2>
             <form onSubmit={addToData}>
-                    <InputForm type="text" name="firstName" placeholder="First Name"/>
-                    <InputForm type="text" name="lasttName" placeholder="Last Name"/>
-                    <InputForm type="date" name="birthdate" placeholder="Date of birth"/>
-                    <InputForm type="email" name="email" placeholder="Email"/>
-                    <InputForm autocomplete='on' type="password" name="password" placeholder="Password"/>
-                    <InputForm autocomplete='on' type="password" name="confirmPassword" placeholder="Confirm password"/>
+                    <InputForm 
+                        type="text" 
+                        name="firstName" 
+                        placeholder="First Name"
+                    />
+                    <InputForm 
+                        type="text" 
+                        name="lastName" 
+                        placeholder="Last Name"
+                    />
+                    <InputForm 
+                        type="date" 
+                        name="birthdate" 
+                        placeholder="Date of birth"
+                    />
+                    <InputForm 
+                        type="email" 
+                        name="email" 
+                        placeholder="Email"
+                    />
+                    <InputForm 
+                        autocomplete='on' 
+                        type="password" 
+                        name="password" 
+                        placeholder="Password"
+                    />
+                    <InputForm 
+                        autocomplete='on' 
+                        type="password" 
+                        name="confirmPassword" 
+                        placeholder="Confirm password"
+                    />
+                    
                     <Button type='submit'>Register</Button>
             </form>
+
         </Container>
     )
     }
--- src/components/UsersData.jsx
+++ src/components/UsersData.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 
 export const UsersData = () => {
     const data = useSelector(state => state.addUser.data)
-    let dataMap = data.map(arr => (<p key={arr.id}>{arr.firstName} {arr.lastName}</p>))
+    let dataMap = data.map(arr => (<p key={arr.lastName}>{arr.firstName} {arr.lastName}</p>))
   return (
     <div>
       <h3>UsersData:</h3>
