6beb65520ac9bbcb0b33fdf0663daafb05491559
Roman-cas88 <orekhovskiy.roman@gmail.com>
2023-03-23 12:38:08 +0100

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputForm } from './InputForm'
+import { BsEyeSlash, BsEye } from 'react-icons/bs'
+
 
 export const Login = () => {
-    const submit = () => {
 
+    const submit = (e) => {
+        e.preventDefault();
+        console.log('check');
     }
+    const [type, setType] = useState("password")
+
+    const showPassword = () => {
+        type === 'password' ? setType('text') : setType('password')
+    }
+
   return (
     <div className='login'>
         <h3>Log in:</h3>
@@ -15,12 +25,20 @@ export const Login = () => {
                 label="Email"
                 placeholder="Email"
                 />
-            <InputForm 
-                type="password" 
-                name="password" 
-                label="Password"
-                placeholder="Password"
-            />
+            <div className='password'>
+                <InputForm 
+                    type={type} 
+                    name="password" 
+                    label="Password"
+                    errMessage="Password should be 8-20 characters and include at least 1 letter and 1 number!"
+                    pattern="^(?=.*[0-9])(?=.*[a-za-z])(?=.*[A-ZA-Z])[a-zA-Z0-9!@#$%^&*()-_)]{8,20}$"
+                    placeholder="Password"
+                    />
+                <div className='showPassword' onClick={showPassword}>
+                    {type === "password" && <BsEye size={15}/>}
+                    {type === "text" && <BsEyeSlash size={15}/>}
+                </div>
+            </div>
             <button className='submitButton' type='submit'>Login</button>
         </form>
         
